e6df3f2279c098235af2e72948196afde11fe064
luckysahani <luckythegreat4@gmail.com>
2014-09-12 03:36:39 +0530

    added the ratio

--- Project/src/main.c
+++ Project/src/main.c
@@ -23,7 +23,7 @@ Table *table;
 
 /* some lighting */
 GLfloat ambientLightA[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
-GLfloat diffuseLightA[4] = { 0.8f, 0.8f, 0.8f, 1.0f };
+GLfloat diffuseLightA[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
 GLfloat ambientLightB[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 GLfloat diffuseLightB[4] = { 0.8f, 0.8f, 0.8f, 1.0f };
@@ -57,39 +57,39 @@ void init() {
 		block[player][14] = create_block(PAWN_TYPE_QUEEN, player);
 		block[player][15] = create_block(PAWN_TYPE_KING, player);
 	}
-	// for (i1 = 0; i1 < 15; ++i1)
-	// {
-	// 	glmScale(block[PLAYER_TYPE_WHITE][i1]->model,0.6);
-	// 	glmScale(block[PLAYER_TYPE_BLACK][i1]->model,0.6);
+	for (i1 = 0; i1 < 15; ++i1)
+	{
+		glmScale(block[PLAYER_TYPE_WHITE][i1]->model,0.6);
+		glmScale(block[PLAYER_TYPE_BLACK][i1]->model,0.6);
 	}
 	//glmScale(block[PLAYER_TYPE_WHITE][12],0.5);
 	
 	/* place all the pieces */
 	int x, y;
 	/* -- white -- */
-	y=0; chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][8],  CELL(0, y)); /* rooks */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][9],  CELL(7, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][10], CELL(1, y)); /* knight */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][11], CELL(6, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][12], CELL(2, y)); /* bishop */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][13], CELL(5, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][14], CELL(3, y)); /* queen */
-	     //chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][15], CELL(4, y)); /* king */
-	y=1; for (x=0; x<8; x++) chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][x], CELL(x, y));
+	 y=rand()/RAND_MAX*8; chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][8],  CELL(0, y));  /*rooks */
+	 y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][9],  CELL(7, y));
+	// y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][10], CELL(1, y)); /* knight */
+	// y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][11], CELL(6, y));
+	// y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][12], CELL(2, y)); /* bishop */
+	 y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][13], CELL(5, y));
+	 y=rand()/RAND_MAX*8;     chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][14], CELL(3, y)); /* queen */
+	//      //chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][15], CELL(4, y)); /* king */
+	// y=rand()/RAND_MAX*8; for (x=0; x<5; x++){y=rand()/RAND_MAX*8; chessboard_place_block(chessboard, block[PLAYER_TYPE_WHITE][x], CELL(x, y));}
 	
 	/* -- black -- */
-	y=7; chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][8],  CELL(0, y)); /* rooks */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][9],  CELL(7, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][10], CELL(1, y)); /* knight */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][11], CELL(6, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][12], CELL(2, y)); /* bishop */
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][13], CELL(5, y));
-	     chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][14], CELL(3, y)); /* queen */
-	     //chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][15], CELL(4, y)); /* king */
-	y=6; for (x=0; x<8; x++) chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][x], CELL(x, y));
+	// y=7; chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][8],  CELL(0, y)); /* rooks */
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][9],  CELL(7, y));
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][10], CELL(1, y)); /* knight */
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][11], CELL(6, y));
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][12], CELL(2, y)); /* bishop */
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][13], CELL(5, y));
+	//      chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][14], CELL(3, y)); /* queen */
+	//      //chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][15], CELL(4, y)); /* king */
+	// y=6; for (x=0; x<2; x++) chessboard_place_block(chessboard, block[PLAYER_TYPE_BLACK][x], CELL(x, y));
 	
     /* opengl initialisations */
-	glClearColor (0.0, 0.0, 1.0, 1.0);
+	glClearColor (0.0f,0.2f,0.2f, 1.0);
 	glShadeModel (GL_SMOOTH);
 	glEnable(GL_BLEND);
 	glEnable(GL_DEPTH_TEST);
