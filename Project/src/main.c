a21623fbcc90c3bf5fd6999262e554331971d595
luckysahani <luckythegreat4@gmail.com>
2014-11-10 19:42:34 +0530

    four blocks created successfully

--- Project/src/main.c
+++ Project/src/main.c
@@ -280,7 +280,7 @@ void create_new_shape(int type,int color_block)
 			tetris_board_place_block(tetris_board,current_block, CELL(x[i], y[i],z[i]),z[i]);
 		}
 	}
-	if(type==2)
+	else if(type==2)
 	{
 		int temp_x=rand()%6;
 		int temp_y=rand()%7;
@@ -288,10 +288,44 @@ void create_new_shape(int type,int color_block)
 		x[1]=temp_x + 1;
 		x[2]=x[3]=temp_x+2;
 		y[3]=temp_y;
-		// y[1]=temp_y
 		y[0]=y[1]=y[2]=temp_y+1;
 		z[0]=z[1]=z[2]=z[3]=8;
-		// z[3]=8;
+		for ( i = 0; i < 4; ++i)
+		{
+			temp_block=create_block(squareshape, 1);
+			current_block=set_block(global_type_block, color_block,temp_block);
+			tetris_board_place_block(tetris_board,current_block, CELL(x[i], y[i],z[i]),z[i]);
+		}
+		
+	}
+	else if(type==3)
+	{
+		int temp_x=rand()%6;
+		int temp_y=rand()%7;
+		x[0]=temp_x;
+		x[1]=x[3]=temp_x + 1;
+		x[2]=temp_x+2;
+		y[3]=temp_y;
+		y[0]=y[1]=y[2]=temp_y+1;
+		z[0]=z[1]=z[2]=z[3]=8;
+		for ( i = 0; i < 4; ++i)
+		{
+			temp_block=create_block(squareshape, 1);
+			current_block=set_block(global_type_block, color_block,temp_block);
+			tetris_board_place_block(tetris_board,current_block, CELL(x[i], y[i],z[i]),z[i]);
+		}
+		
+	}
+	else if(type==4)
+	{
+		int temp_x=rand()%5;
+		int temp_y=rand()%8;
+		x[0]=temp_x;
+		x[1]=temp_x + 1;
+		x[2]=temp_x + 2;
+		x[3]=temp_x + 3;
+		y[0]=y[1]=y[2]=y[3]=temp_y;
+		z[0]=z[1]=z[2]=z[3]=8;
 		for ( i = 0; i < 4; ++i)
 		{
 			temp_block=create_block(squareshape, 1);
@@ -345,36 +379,19 @@ void increment_board_status(int type)
 	if(!collision()) return;
 	printf("Updating board status\n");
 	int i,j;
-	if(type==1)
-	{
-		// printf("entered\n");
+	// if(type==1)
+	// {
 		for ( i = 0; i < 4; ++i)
 		{
 			board_status[x[i]][y[i]]=z[i]+1;
 			view_status[x[i]][y[i]][z[i]]=1;
 			placed_status[x[i]][y[i]][z[i]]=1;
 		}
-	}
-	else if(type==2)
-	{
-		for ( i = 0; i < 4; ++i)
-		{
-			board_status[x[i]][y[i]]=z[i]+1;
-			view_status[x[i]][y[i]][z[i]]=1;
-			placed_status[x[i]][y[i]][z[i]]=1;
-		}
-	}
+	// }
 	for(i=0;i<4;++i)
 	{
 		printf("x = %d,y=%d,z=%d,board_status=%d,view_prev=%dand viewstatus=%d\n",x[i],y[i],z[i],board_status[x[i]][y[i]],view_status[x[i]][y[i]][z[i]-1],view_status[x[i]][y[i]][z[i]] );
 	}
-	// for ( i = 0; i < 4; ++i)
-	// {
-	// 	for ( j = 0; j < z[i]; ++j)
-	// 	{
-	// 		printf("view status=%d\n",view_status[x[i]][y[i]][j]);
-	// 	}
-	// }
 }
 
 //The main code is implemented here
@@ -389,8 +406,8 @@ void update_game()
 		printf("\n\n");
 		flag=0;
 		// count=height/0.1;
-		// type= rand()%3;
-		type=2;
+		type= rand()%4 +1;
+		// type=2;
 		global_type=type;
 		color_block=rand()%3;
 		printf("Creating the blocks\n");
